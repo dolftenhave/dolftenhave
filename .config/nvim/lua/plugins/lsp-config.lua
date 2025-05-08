2278bcb08adae42f2bae4eb73126f33f27316e94
dolft <dolftenhave@gmail.com>
2025-05-08 12:13:16 +1200

    Added rust to the lsp

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,41 +1,42 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				-- Add all lsp servers here
-				ensure_installed = {
-					"lua_ls", -- lua
-					"jdtls", -- java
-					--					"bashls", -- bash
-					"clangd", -- c++
-					"ts_ls",
-					-- find more on the mason github page
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.jdtls.setup({ capabilities = capabilities })
-			--			lspconfig.bashls.setup({})
-			lspconfig.clangd.setup({ capabilities = capabilities })
-			lspconfig.ts_ls.setup({ capabilities = capabilities })
-
-			--Custom binds
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+  {
+    'williamboman/mason.nvim',
+    config = function()
+      require('mason').setup()
+    end,
+  },
+  {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require('mason-lspconfig').setup {
+        -- Add all lsp servers here
+        ensure_installed = {
+          'lua_ls', -- lua
+          'jdtls', -- java
+          --					"bashls", -- bash
+          'clangd', -- c++
+          'ts_ls',
+          -- find more on the mason github page
+          'rust_analyzer',
+        },
+      }
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local lspconfig = require 'lspconfig'
+      lspconfig.lua_ls.setup { capabilities = capabilities }
+      lspconfig.jdtls.setup { capabilities = capabilities }
+      --			lspconfig.bashls.setup({})
+      lspconfig.clangd.setup { capabilities = capabilities }
+      lspconfig.ts_ls.setup { capabilities = capabilities }
+      lspconfig.rust_analyzer.setup { capabilities = capabilities }
+      --Custom binds
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+    end,
+  },
 }
--- backup_configs.sh
+++ backup_configs.sh
@@ -2,5 +2,5 @@
 
 # This script backs up tmux and nvim to the current file
 
-cp -r ~/.config/tmux/* ./.config/
-cp -r ~/.config/nvim/* ./.config/
+cp -r ~/.config/tmux/* ./.config/tmux/
+cp -r ~/.config/nvim/* ./.config/nvim/
